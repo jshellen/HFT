8e447c943bbfb340a9bc67896072e2c036f5897b
Juha Hellen <juha.hellen@aalto.fi>
2020-07-19 13:58:04 +0300

    add: src and notebook

---
+++ main.py
@@ -0,0 +1,97 @@
+import numpy as np
+import matplotlib.pyplot as pp;
+
+from src.hjb_solvers import (
+    MM_Model_Parameters,
+    AS3P_Finite_Difference_Solver,
+)
+
+
+def create_as3p_model_solutions():
+    
+    lambda_m = 50
+    lambda_p = 50
+    kappa_m = 100
+    kappa_p = 100
+    delta = 0
+    phi = 0.000001
+    alpha = 0.0001
+    q_min = -25
+    q_max = 25
+    cost = 0.001
+    T = 5 # minutes
+    n = 5*60 # one step per second
+    
+    
+    d_grid = np.linspace(0, 0.1, 100)
+    fig, ax = pp.subplots(figsize=[3.5, 3]);
+    pp.plot(d_grid, lambda_p*np.exp(-kappa_p*d_grid),
+            color='blue', lw=3)
+    ax.set_xlabel('distance from mid')
+    ax.set_ylabel('fill rate (fills/minute)')
+    
+    parameters = MM_Model_Parameters(lambda_m, lambda_p ,kappa_m, kappa_p, delta,
+                                   phi, alpha, q_min, q_max, T, cost)
+    
+    impulses, model = AS3P_Finite_Difference_Solver.solve(parameters, N_steps=n)
+    
+    # Plot the value function
+    Y = model.q_grid
+    X = model.t_grid
+    X, Y = np.meshgrid(X,Y)
+    f = pp.figure(figsize=[5, 4]);
+    pp3d = pp.axes(projection="3d", elev=20, azim=50);
+    pp3d.set_title("Value function");
+    pp3d.set_xlabel("Minute");
+    pp3d.set_ylabel("Inventory");
+    pp3d.set_zlabel("Value");
+    pp3d.plot_surface(X, Y, model.h, cmap='magma');
+    f.savefig("./graphs/value_function.pdf", bbox_inches='tight')
+    
+    # Plot the impulse regions
+    from matplotlib import colors
+    import matplotlib.patches as mpatches
+    mycolors = ['white', 'blue', 'red']
+    cmap = colors.ListedColormap(mycolors)
+    
+    f, ax = pp.subplots(figsize=[4, 4])
+    ax.imshow(impulses, cmap=cmap, aspect='auto')
+    ax.set_xticks([0, 0.5*n, n])
+    ax.set_xticklabels([0, int(0.5*n), n],fontsize=8);
+    ax.set_yticks(np.arange(0, len(model.q_grid), 2));
+    ax.set_yticklabels(model.q_grid[::2],fontsize=8);
+    ax.set_ylabel('Inventory')
+    ax.set_xlabel('Second')
+    f.savefig("./graphs/impulse_regions.pdf", bbox_inches='tight')
+    
+    # Plot the ask spread for continuation region
+    f, ax = pp.subplots(figsize=[5, 4])
+    for q in range(3, -4, -1):
+        ax.plot(model.l_p[q], label=f'q={q}')
+    ax.set_title("Ask to mid spread")
+    pp.legend()
+    f.savefig("./graphs/ask_to_mid_spread.pdf", bbox_inches='tight')
+
+    # Plot the bid spread for continuation region
+    fig, ax = pp.subplots(figsize=[5, 4])
+    for q in range(3, -4, -1):
+        ax.plot(model.l_m[q], label=f'q={q}')
+    ax.set_title("Bid to mid spread")
+    pp.legend()
+    f.savefig("./graphs/bid_to_mid_spread.pdf", bbox_inches='tight')
+    
+    pp.show()
+    
+    
+if __name__ == '__main__':
+    create_as3p_model_solutions()
+
+
+
+
+
+
+
+
+
+
---
+++ src/hjb_solvers.py
@@ -0,0 +1,435 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from enum import Enum
+from copy import deepcopy
+
+__author__      = "Juha-Samuli Hellen"
+
+class MM_Model_Parameters:
+
+    def __init__(self, lambda_m, lambda_p, kappa_m, kappa_p, delta, phi, alpha, q_min, q_max, T, cost):
+        
+        if(not isinstance(lambda_m,(float,int,np.int32,np.int64))):
+            raise TypeError(f'lambda_m has to be type of <float> or <int>, not {type(lambda_m)}')
+
+        if(not isinstance(lambda_p,(float,int,np.int32,np.int64))):
+            raise TypeError(f'lambda_p has to be type of <float> or <int>, not {type(lambda_m)}')       
+
+        if(not isinstance(delta,(float,int,np.int32,np.int64))):
+            raise TypeError('delta has to be type of <float> or <int>')
+
+        if(not isinstance(phi,(float,int,np.int32,np.int64))):
+            raise TypeError('phi has to be type of <float> or <int>')
+
+        if(not isinstance(alpha,(float,int,np.int32,np.int64))):
+            raise TypeError('alpha has to be type of <float> or <int>')            
+
+        if(not isinstance(q_min,(int,np.int32,np.int64))):
+            raise TypeError('q_min has to be type of <int>')  
+            
+        if(not isinstance(q_max,(int,np.int32,np.int64))):
+            raise TypeError('q_max has to be type of <int>')  
+        
+        if(q_max <= q_min):
+            raise ValueError('q_max has to be larger than q_min!')
+        
+        self.m_lambda_m = lambda_m # Order-flow at be bid
+        self.m_lambda_p = lambda_p # Order flow at the offer
+        self.m_kappa_m = lambda_m # Order-flow decay at be bid
+        self.m_kappa_p = lambda_p # Order flow decay at the offer
+        self.m_T = T
+        
+        self.m_delta = delta # average "edge" with respect to mid-price
+        self.m_phi = phi # running inventory penalty 
+        self.m_alpha = alpha # terminal inventory penalty
+        
+        self.m_q_min = q_min 
+        self.m_q_max = q_max
+        self.m_cost = cost
+    
+    @property
+    def lambda_m(self):
+        """
+        Return copy of the order flow parameter at the bid
+        """
+        return deepcopy(self.m_lambda_m)
+    
+    @property
+    def lambda_p(self):
+        """
+        Return copy of the order flow parameter at the ask
+        """        
+        return deepcopy(self.m_lambda_p)
+
+    @property
+    def kappa_m(self):
+        """
+        Return copy of the order flow parameter at the bid
+        """
+        return deepcopy(self.m_kappa_m)
+
+    @property
+    def kappa_p(self):
+        """
+        Return copy of the order flow parameter at the ask
+        """        
+        return deepcopy(self.m_kappa_p)
+    
+    @property
+    def T(self):
+        """
+        Return copy of the order flow parameter at the ask
+        """        
+        return deepcopy(self.m_T)
+    
+    @property
+    def delta(self):
+        """
+        Return copy of the average "edge".
+        """        
+        return deepcopy(self.m_delta)
+
+    @property
+    def phi(self):
+        """
+        Return copy of the running inventory penalty.
+        """        
+        return deepcopy(self.m_phi)
+
+    @property
+    def alpha(self):
+        """
+        Return copy of the terminal inventory penalty.
+        """        
+        return deepcopy(self.m_alpha)
+
+    @property
+    def q_min(self):
+        """
+        Return copy of the minimum inventory level.
+        """        
+        return deepcopy(self.m_q_min)    
+
+    @property
+    def q_max(self):
+        """
+        Return copy of the maximum inventory level.
+        """        
+        return deepcopy(self.m_q_max) 
+    
+    @property
+    def cost(self):
+        """
+        Return copy of the trading cost.
+        """        
+        return deepcopy(self.m_cost) 
+    
+class AS_Model_Output:
+ 
+    def __init__(self, l_p,  l_m,h, q_lookup, q_grid, t_grid, N_steps, params):
+        
+        self.m_l_p = l_p
+        self.m_l_m = l_m
+        self.m_h = h
+        
+        self.m_q_lookup = q_lookup
+        self.m_q_grid = q_grid
+        self.m_t_grid = t_grid
+        self.m_N_steps = N_steps
+        self.m_params = params
+    
+    @property
+    def params(self):
+        """
+        Returns a deepcopy of the model parameters
+        """
+        return deepcopy(self.m_params)
+    
+    @property
+    def h(self):
+        """
+        Returns a copy of the value function h at each (q,t) node.
+        """
+        return deepcopy(self.m_h)
+    
+    @property
+    def l_p(self):
+        """
+        Returns a copy of the decision variables l^{+} at each (q,t) node
+        """
+        return deepcopy(self.m_l_p)
+ 
+    @property
+    def l_m(self):
+        """
+        Returns a copy of the decision variables l^{-} at each (q,t) node
+        """
+        return deepcopy(self.m_l_m)
+    
+    @property
+    def q_grid(self):
+        """
+
+        """
+        return deepcopy(self.m_q_grid)
+        
+    @property
+    def t_grid(self):
+        """
+
+        """
+        return deepcopy(self.m_t_grid)
+        
+        
+    @property
+    def N_steps(self):
+        
+        return deepcopy(self.m_N_steps)
+    
+    def get_l_plus(self,q,t):
+        """
+        Returns SELL decision given inventory q and time remaining till end of
+        trading day t.
+        """
+        t_idx = filter(lambda x: x>=t, self.m_t_grid)[0]
+        
+        if(q in self.m_q_lookup):
+            q_idx = self.m_q_lookup[q]
+        else:
+            raise KeyError(f'Inventory level {q} exceeds inventory grid used in pre-computation.')
+            
+        return self.m_l_p[t_idx,q_idx]
+
+    def get_l_minus(self,q,t):
+        """
+        Returns BUY decision given inventory q and time remaining till end of
+        trading day t.
+        """        
+        t_idx = filter(lambda x: x>=t, self.m_t_grid)[0]        
+        if(q in self.m_q_lookup):
+            q_idx = self.m_q_lookup[q]
+        else:
+            raise KeyError(f'Inventory level {q} exceeds inventory grid used in pre-computation.')
+        
+        return self.m_l_m[t_idx,q_idx]
+       
+_EFF_ZERO = 1E-10
+
+class AS2P_Finite_Difference_Solver:
+    """
+    Avellaneda Stoikov ++ model with terminal and running inventory penalties
+    """
+    @staticmethod
+    def solve(params, N_steps = 500):
+        """
+        Solves the optimal bid and ask spreads for a mm algorithm 
+        using backward Euler finite difference scheme.
+        """    
+        n = params.q_max - params.q_min + 1 
+        q_grid = [q for q in range(params.q_max, params.q_min-1, -1)]
+        q_map = dict( (q, i) for i, q in enumerate(q_grid))
+        q_lookup = lambda q : q_map[q] 
+        
+        C1 = params.lambda_p / (np.e * params.kappa_p)
+        C2 = params.lambda_m / (np.e * params.kappa_m)
+    
+        # Terminal time
+        T = params.T
+        
+        # Time step for finite difference
+        dt = T/N_steps
+        
+        # Value function 
+        h = np.zeros((n, N_steps))
+        h[:, -1] = np.array([-params.alpha*q**2 for q in q_grid])
+        
+        # Time points
+        t_grid = np.zeros(N_steps)
+        t_grid[-1] = T
+        
+        # Compute 
+        for idx in range(N_steps-1, 0, -1):
+            
+            # Update time 
+            t_grid[idx-1] = t_grid[idx] - dt
+            
+            # Value function vector for current time step
+            h_cur = np.zeros(n)
+            
+            # Value function vector for previous time step
+            h_prev = h[:, idx]
+            
+            # Loop all inventory levels
+            for q in range(params.q_max, params.q_min-1, -1):
+                if q == params.q_max:
+                    h_cur[q_lookup(q)] = h_prev[q_lookup(q)] + (-(params.phi*q**2)
+                                            + C1 * np.exp(params.kappa_p*(h_prev[q_lookup(q-1)]-h_prev[q_lookup(q)])))*dt
+                elif q == params.q_min:
+                    h_cur[q_lookup(q)] = h_prev[q_lookup(q)] + (-(params.phi*q**2) 
+                                         + C2 * np.exp(params.kappa_m*(h_prev[q_lookup(q+1)]-h_prev[q_lookup(q)])))*dt
+                else:
+                    h_cur[q_lookup(q)] = h_prev[q_lookup(q)] + (-(params.phi*q**2) 
+                                         + C1 * np.exp(params.kappa_p*(h_prev[q_lookup(q-1)]-h_prev[q_lookup(q)])) 
+                                         + C2 * np.exp(params.kappa_m*(h_prev[q_lookup(q+1)]-h_prev[q_lookup(q)])))*dt
+            
+            # Set euler approximated value for h_cur
+            h[:, idx-1] = h_cur
+
+        # Solve optimal bid-ask spreads
+        d_p = {}
+        d_m = {}
+        for q in range(params.q_max, params.q_min, -1):
+            d_p[q] = (h[q_lookup(q)] - h[q_lookup(q-1)]) + (1. / params.kappa_p)
+
+        for q in range(params.q_min, params.q_max):
+            d_m[q] = (h[q_lookup(q)] - h[q_lookup(q+1)]) + (1. / params.kappa_m)
+        
+        return AS_Model_Output(d_p, d_m, h, q_lookup, q_grid, t_grid, N_steps, params)
+
+class AS3P_Finite_Difference_Solver:
+    """
+    Avellaneda Stoikov +++ model with terminal and running inventory penalties
+    and hedging
+    
+    @ Juha Hellén
+    """
+    @staticmethod
+    def solve(params, N_steps = 500):
+        """
+        Runs implicit backward Euler finite difference scheme for HJB part
+        and then solves the double obstacle QVI problem given information
+        of the value function h
+        
+        
+        (1) Solve HJB assuming no impulses
+        (2) Run again satisfying QVIs
+        (3) For continuation region solve the bid ask spread
+        """    
+        
+        n = params.q_max - params.q_min + 1 
+        q_grid = [q for q in range(params.q_max, params.q_min-1, -1)]
+        q_map = dict( (q, i) for i, q in enumerate(q_grid))
+        q_lookup = lambda q : q_map[q] 
+        
+        C1 = params.lambda_p / (np.e * params.kappa_p)
+        C2 = params.lambda_m / (np.e * params.kappa_m)
+    
+        # Terminal time
+        T = params.T
+        
+        # Time step for finite difference
+        dt = T/N_steps
+        
+        # Function h in V(t, x) = x_t + s_t * q_t + h(t, q) 
+        h_hjb = np.zeros((n, N_steps))
+        
+        h_hjb[:, -1] = np.array([-params.alpha*q**2 for q in q_grid])
+        
+        # Time points (it is useful to know the time points for which we have
+        # approximated the h(t, x)
+        t_grid = np.zeros(N_steps)
+        t_grid[-1] = T
+        
+        # (1) Solve HJB by running the backward euler
+        for idx in range(N_steps-1, 0, -1):
+            
+            # Update time 
+            t_grid[idx-1] = t_grid[idx] - dt
+            
+            # Value function vector for current time step
+            h_cur = np.zeros(n)
+            
+            # Value function vector for previous time step
+            h_prev = h_hjb[:, idx]
+            
+            # Loop all inventory levels
+            for q in range(params.q_max, params.q_min-1, -1):
+                
+                # At upper inventory limit post only sell LO
+                if q == params.q_max:
+                    h_cur[q_lookup(q)] = h_prev[q_lookup(q)] + (-(params.phi*q**2)
+                                            + C1 * np.exp(params.kappa_p*(
+                                                h_prev[q_lookup(q-1)]-h_prev[q_lookup(q)])))*dt
+                    
+                # At lower inventory limit post only buy LO
+                elif q == params.q_min:
+                    h_cur[q_lookup(q)] = h_prev[q_lookup(q)] + (-(params.phi*q**2) 
+                                         + C2 * np.exp(params.kappa_m*(
+                                             h_prev[q_lookup(q+1)]-h_prev[q_lookup(q)])))*dt
+                # Post both sides
+                else:
+                    h_cur[q_lookup(q)] = h_prev[q_lookup(q)] + (-(params.phi*q**2) 
+                                         + C1 * np.exp(params.kappa_p*(
+                                             h_prev[q_lookup(q-1)]-h_prev[q_lookup(q)])) 
+                                         + C2 * np.exp(params.kappa_m*(
+                                             h_prev[q_lookup(q+1)]-h_prev[q_lookup(q)])))*dt
+            
+            # Set euler approximated value for h_cur
+            h_hjb[:, idx-1] = h_cur
+        
+        
+        
+        # (2) Run QVI checking: max(HJB part, Send buy MO, Send sell MO)
+        h = h_hjb.copy()
+        impulses = np.zeros((n, N_steps))
+        for idx in range(N_steps-1, 0, -1):
+            
+            # Vector for impulses
+            i_ = np.zeros(n)
+            
+            # Vector for HJB-QVI consistent h
+            h_qvi = np.zeros(n)
+            
+            # Loop all inventory levels
+            for q in range(params.q_max, params.q_min-1, -1):
+                
+                qvi_values = -100000000000*np.ones(3)
+                
+                # Do nothing - let the system evolve according to HJB
+                qvi_values[0] = h[q_lookup(q), idx]
+                
+                # Inventory at the upper boundary
+                # Check if we send a sell MO
+                if q == params.q_max:
+                    
+                    # Hedge by sending sell MO
+                    qvi_values[2] = h[q_lookup(q-1), idx] - params.cost
+                    
+                # Inventory at the lower boundary
+                # Check if we send a buy MO    
+                elif q == params.q_min:
+                    
+                    # Hedge by sending buy MO
+                    qvi_values[1] = h[q_lookup(q+1), idx] - params.cost
+                
+                # Inventory is between upper and lower boundaries
+                # Just solve the HJB-QVI    
+                else:
+                    
+                    # Hedge by sending buy MO
+                    qvi_values[1] = h[q_lookup(q+1), idx] - params.cost
+                    
+                    # Hedge by sending sell MO
+                    qvi_values[2] = h[q_lookup(q-1), idx] - params.cost
+                    
+                
+                # Satisfy HJB-QVI conditions
+                h_qvi[q_lookup(q)] = np.max(qvi_values)    
+                i_[q_lookup(q)] = np.argmax(qvi_values)
+                
+            h_hjb[:, idx] = h_qvi
+            impulses[:, idx] = i_
+        
+        
+        # (3) Solve optimal bid-ask spreads for the continuation region
+        d_p = {}
+        d_m = {}
+        for q in range(params.q_max, params.q_min, -1):
+            d_p[q] = (h[q_lookup(q)] - h[q_lookup(q-1)]) + (1. / params.kappa_p)
+
+        for q in range(params.q_min, params.q_max):
+            d_m[q] = (h[q_lookup(q)] - h[q_lookup(q+1)]) + (1. / params.kappa_m)
+        
+        return impulses, AS_Model_Output(d_p, d_m, h, q_lookup, q_grid, t_grid, N_steps, params)    
+    
+
